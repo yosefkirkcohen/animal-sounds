1e12816737bbcf5f0389f87a3f7f352c5d492581
yosefkirkcohen <ycohen45@yahoo.com>
2021-07-15 12:03:27 -0700

    not great

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
+const horseEl = document.getElementById('horse-image')
+const dogEl = document.getElementById('dog-image')
+const catEl = document.getElementById('cat-image')
+const horseSound =document.getElementById('horse-sound')
+const dogSound =document.getElementById('dog-sound')
+const catSound =document.getElementById('cat-sound')
 
+console.log(horseEl)
 // initialize state
 
 // set event listeners 
+horseEl.addEventListener('click', () => {
+  horseSound.play();
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
